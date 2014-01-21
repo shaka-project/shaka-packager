257b48536ab6a43ec38caba4e4490f290149718b
Kongqun Yang <kqyang@google.com>
2014-01-21 12:36:27 -0800

    Clean up tools directory.
    Remove all files and directories except:
    clang  emacs  git  gyp  heapcheck  protoc_wrapper  valgrind  vim
    xdisplaycheck
    
    Change-Id: I6326e4edad4b843e0d0c2ef39c20ac90f13c8579

--- tools/android/asan/asanwrapper.sh
+++
@@ -1,9 +0,0 @@
-#!/system/bin/sh
-# Copyright (c) 2012 The Chromium Authors. All rights reserved.
-# Use of this source code is governed by a BSD-style license that can be
-# found in the LICENSE file.
-
-ASAN_OPTIONS=debug=1,verbosity=1,strict_memcmp=0 \
-LD_LIBRARY_PATH=/data/local/tmp/asan:$LD_LIBRARY_PATH \
-LD_PRELOAD=libclang_rt.asan-arm-android.so \
-exec $@
--- tools/perf_expectations/README.txt
+++
@@ -1,2 +0,0 @@
-For instructions see
-http://www.chromium.org/developers/tree-sheriffs/perf-sheriffs
