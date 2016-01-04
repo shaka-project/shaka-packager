9fcb18559e88eda5b070c080fa48f4079ed3e2c3
Widevine Buildbot <widevine-device-buildbot@google.com>
2016-01-04 23:24:50 +0000

    Generate SDK documents for commit 8a0458c17bcf0500118ca6a4792e597a70ec79f5

--- docs/search/all_3.js
+++ docs/search/all_3.js
@@ -13,9 +13,10 @@ var searchData=
   ['decryptconfig',['DecryptConfig',['../d4/da1/classedash__packager_1_1media_1_1DecryptConfig.html',1,'edash_packager::media']]],
   ['decryptconfig',['DecryptConfig',['../d4/da1/classedash__packager_1_1media_1_1DecryptConfig.html#a8241c75bb72af0b2de61ae1e276692ed',1,'edash_packager::media::DecryptConfig']]],
   ['decryptorsource',['DecryptorSource',['../dc/df1/classedash__packager_1_1media_1_1DecryptorSource.html',1,'edash_packager::media']]],
-  ['delete',['Delete',['../de/d19/classedash__packager_1_1media_1_1File.html#a7e0fcd3c439e810e8e17baf32dcc75af',1,'edash_packager::media::File::Delete()'],['../dd/d98/classedash__packager_1_1media_1_1LocalFile.html#a95e4a5eff87224c9a156b5e2c7824cb9',1,'edash_packager::media::LocalFile::Delete()']]],
-  ['demuxer',['Demuxer',['../d5/dce/classedash__packager_1_1media_1_1Demuxer.html',1,'edash_packager::media']]],
+  ['delete',['Delete',['../de/d19/classedash__packager_1_1media_1_1File.html#a7e0fcd3c439e810e8e17baf32dcc75af',1,'edash_packager::media::File::Delete()'],['../dd/d98/classedash__packager_1_1media_1_1LocalFile.html#a95e4a5eff87224c9a156b5e2c7824cb9',1,'edash_packager::media::LocalFile::Delete()'],['../d3/d81/classedash__packager_1_1media_1_1MemoryFile.html#a039f71274ee7607087a43d3be67fd6f8',1,'edash_packager::media::MemoryFile::Delete()']]],
+  ['deleteall',['DeleteAll',['../d3/d81/classedash__packager_1_1media_1_1MemoryFile.html#a85b276d2b2d58c6b6a4d599906a510de',1,'edash_packager::media::MemoryFile']]],
   ['demuxer',['Demuxer',['../d5/dce/classedash__packager_1_1media_1_1Demuxer.html#a5305b2b16956b7ab973eeded4232226e',1,'edash_packager::media::Demuxer']]],
+  ['demuxer',['Demuxer',['../d5/dce/classedash__packager_1_1media_1_1Demuxer.html',1,'edash_packager::media']]],
   ['demuxstreamidmediasample',['DemuxStreamIdMediaSample',['../d0/dfa/structedash__packager_1_1media_1_1wvm_1_1DemuxStreamIdMediaSample.html',1,'edash_packager::media::wvm']]],
   ['dtsspecific',['DTSSpecific',['../d0/db7/structedash__packager_1_1media_1_1mp4_1_1DTSSpecific.html',1,'edash_packager::media::mp4']]],
   ['duration',['Duration',['../d4/d90/classedash__packager_1_1media_1_1mp4_1_1DecodingTimeIterator.html#a20162e42fe14b8135828f2d8d6cdd2ef',1,'edash_packager::media::mp4::DecodingTimeIterator']]]
--- docs/search/classes_a.js
+++ docs/search/classes_a.js
@@ -7,6 +7,7 @@ var searchData=
   ['mediaparser',['MediaParser',['../dc/d48/classedash__packager_1_1media_1_1MediaParser.html',1,'edash_packager::media']]],
   ['mediasample',['MediaSample',['../d8/d6c/classedash__packager_1_1media_1_1MediaSample.html',1,'edash_packager::media']]],
   ['mediastream',['MediaStream',['../d2/dfd/classedash__packager_1_1media_1_1MediaStream.html',1,'edash_packager::media']]],
+  ['memoryfile',['MemoryFile',['../d3/d81/classedash__packager_1_1media_1_1MemoryFile.html',1,'edash_packager::media']]],
   ['mkvwriter',['MkvWriter',['../d4/d8c/classedash__packager_1_1media_1_1MkvWriter.html',1,'edash_packager::media']]],
   ['mockadaptationset',['MockAdaptationSet',['../d8/d78/classedash__packager_1_1MockAdaptationSet.html',1,'edash_packager']]],
   ['mockmpdbuilder',['MockMpdBuilder',['../db/d2e/classedash__packager_1_1MockMpdBuilder.html',1,'edash_packager']]],
@@ -27,8 +28,8 @@ var searchData=
   ['mpdnotifymuxerlistener',['MpdNotifyMuxerListener',['../d3/d0e/classedash__packager_1_1media_1_1MpdNotifyMuxerListener.html',1,'edash_packager::media']]],
   ['mpdoptions',['MpdOptions',['../d1/d5d/structedash__packager_1_1MpdOptions.html',1,'edash_packager']]],
   ['mpdwriter',['MpdWriter',['../dc/d81/classedash__packager_1_1MpdWriter.html',1,'edash_packager']]],
-  ['multisegmentsegmenter',['MultiSegmentSegmenter',['../df/d4e/classedash__packager_1_1media_1_1webm_1_1MultiSegmentSegmenter.html',1,'edash_packager::media::webm']]],
   ['multisegmentsegmenter',['MultiSegmentSegmenter',['../dc/d7a/classedash__packager_1_1media_1_1mp4_1_1MultiSegmentSegmenter.html',1,'edash_packager::media::mp4']]],
+  ['multisegmentsegmenter',['MultiSegmentSegmenter',['../df/d4e/classedash__packager_1_1media_1_1webm_1_1MultiSegmentSegmenter.html',1,'edash_packager::media::webm']]],
   ['muxer',['Muxer',['../d4/d1d/classedash__packager_1_1media_1_1Muxer.html',1,'edash_packager::media']]],
   ['muxerlistener',['MuxerListener',['../df/de7/classedash__packager_1_1media_1_1MuxerListener.html',1,'edash_packager::media']]],
   ['muxeroptions',['MuxerOptions',['../de/d57/structedash__packager_1_1media_1_1MuxerOptions.html',1,'edash_packager::media']]]
