d3b2a6ca19d40dca6ac3b70038d427761e110c55
Widevine Buildbot <widevine-device-buildbot@google.com>
2016-01-21 19:33:44 +0000

    Generate SDK documents for commit 940c3571aa13d27a2445b4b5bda1b0ac7e906428

--- docs/search/all_9.js
+++ docs/search/all_9.js
@@ -2,12 +2,11 @@ var searchData=
 [
   ['kadtsheadersize',['kADTSHeaderSize',['../dc/dc6/classedash__packager_1_1media_1_1mp4_1_1AACAudioSpecificConfig.html#a200d0b1a945fed01c2ce92a0b2fa9921',1,'edash_packager::media::mp4::AACAudioSpecificConfig']]],
   ['kdecryptionkeysize',['kDecryptionKeySize',['../d4/da1/classedash__packager_1_1media_1_1DecryptConfig.html#a65b9e6b7a1a2308f38e6b435bf8ce5bb',1,'edash_packager::media::DecryptConfig']]],
-  ['kdefaultaudiobufferdurationinms',['kDefaultAudioBufferDurationInMs',['../db/da0/classedash__packager_1_1media_1_1WebMClusterParser.html#a9a9e4f3a5a1360d947528a553397779eadcde56d3670a152494df7b401c6db1dd',1,'edash_packager::media::WebMClusterParser']]],
-  ['kdefaultvideobufferdurationinms',['kDefaultVideoBufferDurationInMs',['../db/da0/classedash__packager_1_1media_1_1WebMClusterParser.html#a9a9e4f3a5a1360d947528a553397779ea32a5cc2c19fb1598071109cad939311a',1,'edash_packager::media::WebMClusterParser']]],
+  ['kdefaultaudiobufferdurationinms',['kDefaultAudioBufferDurationInMs',['../db/da0/classedash__packager_1_1media_1_1WebMClusterParser.html#a48204223314bba946c59c1fbf3fbbf86adcde56d3670a152494df7b401c6db1dd',1,'edash_packager::media::WebMClusterParser']]],
+  ['kdefaultvideobufferdurationinms',['kDefaultVideoBufferDurationInMs',['../db/da0/classedash__packager_1_1media_1_1WebMClusterParser.html#a48204223314bba946c59c1fbf3fbbf86a32a5cc2c19fb1598071109cad939311a',1,'edash_packager::media::WebMClusterParser']]],
   ['keyfetcher',['KeyFetcher',['../d4/d43/classedash__packager_1_1media_1_1KeyFetcher.html',1,'edash_packager::media']]],
   ['keyrotationfragmenter',['KeyRotationFragmenter',['../dc/d8a/classedash__packager_1_1media_1_1mp4_1_1KeyRotationFragmenter.html',1,'edash_packager::media::mp4']]],
   ['keyrotationfragmenter',['KeyRotationFragmenter',['../dc/d8a/classedash__packager_1_1media_1_1mp4_1_1KeyRotationFragmenter.html#a970fbc82214256788fbcf76809de2488',1,'edash_packager::media::mp4::KeyRotationFragmenter']]],
   ['keysource',['KeySource',['../d9/d8b/classedash__packager_1_1media_1_1KeySource.html',1,'edash_packager::media']]],
-  ['kopusframedurationsmu',['kOpusFrameDurationsMu',['../db/da0/classedash__packager_1_1media_1_1WebMClusterParser.html#ab08c27583d9585af4a71d81bcb4f6772',1,'edash_packager::media::WebMClusterParser']]],
   ['korderinvalid',['kOrderInvalid',['../df/d30/classedash__packager_1_1media_1_1ContentEncoding.html#a4d87c9881d554fbeb6999280d2e463d1',1,'edash_packager::media::ContentEncoding']]]
 ];
--- docs/search/variables_2.js
+++ docs/search/variables_2.js
@@ -2,6 +2,5 @@ var searchData=
 [
   ['kadtsheadersize',['kADTSHeaderSize',['../dc/dc6/classedash__packager_1_1media_1_1mp4_1_1AACAudioSpecificConfig.html#a200d0b1a945fed01c2ce92a0b2fa9921',1,'edash_packager::media::mp4::AACAudioSpecificConfig']]],
   ['kdecryptionkeysize',['kDecryptionKeySize',['../d4/da1/classedash__packager_1_1media_1_1DecryptConfig.html#a65b9e6b7a1a2308f38e6b435bf8ce5bb',1,'edash_packager::media::DecryptConfig']]],
-  ['kopusframedurationsmu',['kOpusFrameDurationsMu',['../db/da0/classedash__packager_1_1media_1_1WebMClusterParser.html#ab08c27583d9585af4a71d81bcb4f6772',1,'edash_packager::media::WebMClusterParser']]],
   ['korderinvalid',['kOrderInvalid',['../df/d30/classedash__packager_1_1media_1_1ContentEncoding.html#a4d87c9881d554fbeb6999280d2e463d1',1,'edash_packager::media::ContentEncoding']]]
 ];
