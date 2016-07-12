1610e2e4fdedb1937f2f8fe5d8dc5039f8800362
Widevine Buildbot <widevine-device-buildbot@google.com>
2016-07-12 19:13:07 +0000

    Generate SDK documents for commit f3d35b730cfb1b6d13f2c5c29e2b8ec7da02d9bb

--- docs/search/variables_5.js
+++ docs/search/variables_5.js
@@ -1,5 +1,4 @@
 var searchData=
 [
-  ['packager_5fversion_5fstring',['packager_version_string',['../d0/d27/structshaka_1_1media_1_1MuxerOptions.html#abd063c42f307ad9ec5e61c806f221884',1,'shaka::media::MuxerOptions']]],
   ['private_5fframe',['private_frame',['../d3/dad/structshaka_1_1media_1_1mp4_1_1ID3v2.html#a33c145fa8ed1417799dce66a94f8e95c',1,'shaka::media::mp4::ID3v2']]]
 ];
