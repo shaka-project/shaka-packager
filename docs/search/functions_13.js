5c9c66a1bdfb61f9d3bb612e9a339c89df53b82d
Widevine Buildbot <widevine-device-buildbot@google.com>
2016-08-18 21:18:15 +0000

    Generate SDK documents for commit db92c9a22b46d6fbe5e00f1e3a0ab9117aa59724

--- docs/search/functions_13.js
+++ docs/search/functions_13.js
@@ -5,5 +5,5 @@ var searchData=
   ['validatewidevinecryptoflags',['ValidateWidevineCryptoFlags',['../d8/daf/namespaceshaka.html#ade3cbb360c0dcaacc5667676af885008',1,'shaka']]],
   ['verifysignature',['VerifySignature',['../d9/d0c/classshaka_1_1media_1_1RsaPublicKey.html#a146cc97c8fcacadde1b08c373b766fe4',1,'shaka::media::RsaPublicKey']]],
   ['video_5fdescription',['video_description',['../de/df6/classshaka_1_1media_1_1mp4_1_1TrackRunIterator.html#aee813f01267e89ff00fdb836bb3a159d',1,'shaka::media::mp4::TrackRunIterator']]],
-  ['videostreaminfo',['VideoStreamInfo',['../d6/db1/classshaka_1_1media_1_1VideoStreamInfo.html#a3f4380f77c0846fc07e07df40d5eb618',1,'shaka::media::VideoStreamInfo']]]
+  ['videostreaminfo',['VideoStreamInfo',['../d6/db1/classshaka_1_1media_1_1VideoStreamInfo.html#a3de6a7d65ddd877c513f3047a86db59e',1,'shaka::media::VideoStreamInfo']]]
 ];
