1d2b7081ecb6911236edca58778d862c69fea5e9
Widevine Buildbot <widevine-device-buildbot@google.com>
2016-10-31 21:08:55 +0000

    Generate SDK documents for commit 458fadc6a81a028e61b5e2a5bfe59da5edea8f8e

--- docs/search/all_12.js
+++ docs/search/all_12.js
@@ -2,6 +2,7 @@ var searchData=
 [
   ['udpfile',['UdpFile',['../d7/d7a/classshaka_1_1media_1_1UdpFile.html',1,'shaka::media']]],
   ['udpfile',['UdpFile',['../d7/d7a/classshaka_1_1media_1_1UdpFile.html#a9668328623027330d9552f116d61ae86',1,'shaka::media::UdpFile']]],
+  ['udpoptions',['UdpOptions',['../d4/d8e/classshaka_1_1media_1_1UdpOptions.html',1,'shaka::media']]],
   ['uint8vectortobase64',['Uint8VectorToBase64',['../d8/daf/namespaceshaka.html#a4c7f8f964fec2bb9543e7ee8da9cd994',1,'shaka']]],
   ['update',['Update',['../d0/d93/classshaka_1_1media_1_1Status.html#a74d29f349b4673d2eef69a47f5152fe4',1,'shaka::media::Status']]],
   ['updatecontentprotectionpssh',['UpdateContentProtectionPssh',['../d8/d8e/classshaka_1_1AdaptationSet.html#ae8a28838bf1bbc1ad8f5b80961fe33c2',1,'shaka::AdaptationSet::UpdateContentProtectionPssh()'],['../de/d52/classshaka_1_1Representation.html#a3d17325903aa01f0c8ff7753d94ff330',1,'shaka::Representation::UpdateContentProtectionPssh()']]],
--- docs/search/classes_11.js
+++ docs/search/classes_11.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['udpfile',['UdpFile',['../d7/d7a/classshaka_1_1media_1_1UdpFile.html',1,'shaka::media']]]
+  ['udpfile',['UdpFile',['../d7/d7a/classshaka_1_1media_1_1UdpFile.html',1,'shaka::media']]],
+  ['udpoptions',['UdpOptions',['../d4/d8e/classshaka_1_1media_1_1UdpOptions.html',1,'shaka::media']]]
 ];
