75e1fc317558df097faafb8021b4f3e169d1502c
KongQun Yang <kqyang@google.com>
2017-11-02 12:08:47 -0700

    Added architecture diagram
    Also remove outdated design.md.
    
    Change-Id: I237b374a2fffe348b1daf5f8d1c37c57b4898fde

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -35,6 +35,7 @@ extensions = ['sphinx.ext.autodoc',
               'sphinx.ext.mathjax',
               'sphinx.ext.viewcode',
               'sphinx.ext.githubpages',
+              'sphinxcontrib.plantuml',
               'cloud_sptheme.ext.table_styling',
               'breathe']
 
