feb7951f33bd2dd908bb64e088ece917ad5fb9bb
KongQun Yang <kqyang@google.com>
2017-12-07 14:41:14 -0800

    Add docker files for testing on various linux distros
    Change-Id: Ic05e2dc326a2b70c43132118d78f6d496d8b7e18

---
+++ packager/testing/dockers/test_dockers.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -e
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+PACKAGER_DIR="$(dirname "$(dirname "$(dirname "$(dirname ${SCRIPT_DIR})")")")"
+
+function docker_run() {
+  docker run -v ${PACKAGER_DIR}:/shaka-packager -w /shaka-packager/src my_container "$@"
+}
+
+for docker_file in ${SCRIPT_DIR}/*_Dockerfile ; do
+  docker build -t my_container -f ${docker_file} ${SCRIPT_DIR}
+  docker_run gclient runhooks
+  docker_run ninja -C out/Release
+  docker_run bash -c 'GYP_DEFINES="clang=0" gclient runhooks'
+  docker_run ninja -C out/Release
+done
