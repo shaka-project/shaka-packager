5a913d7be5c1630b9f29c2e0d3ab22a97293656d
Tomohiro IKEDA <rilakkuma.san.xjapan@gmail.com>
2019-07-13 19:27:00 +0900

    feat: Correspond to IPv6 Multicast (Independent on protocol)

--- packager/file/udp_file.cc
+++ packager/file/udp_file.cc
@@ -16,9 +16,12 @@
 
 #include <arpa/inet.h>
 #include <errno.h>
+#include <netinet/in.h>
+#include <netdb.h>
 #include <strings.h>
 #include <sys/socket.h>
 #include <sys/time.h>
+#include <sys/types.h>
 #include <unistd.h>
 #define INVALID_SOCKET -1
 
@@ -31,6 +34,7 @@
 #endif  // defined(OS_WIN)
 
 #include <limits>
+#include <cassert>
 
 #include "packager/base/logging.h"
 #include "packager/file/udp_options.h"
@@ -41,6 +45,80 @@ namespace {
 
 bool IsIpv4MulticastAddress(const struct in_addr& addr) {
   return (ntohl(addr.s_addr) & 0xf0000000) == 0xe0000000;
+}
+
+bool IsIpv6MulticastAddress(const struct in6_addr& addr) {
+  if (addr.s6_addr[0] == 0xff) {
+    return true;
+  }
+
+  return false;
+}
+
+bool SpecificMalticast(int ai_family,
+                       int level,
+                       SOCKET socket,
+                       struct group_source_req& gsr,
+                       std::unique_ptr<UdpOptions>&& options) {
+  if (inet_pton(ai_family,
+                options->interface_address().c_str(),
+                &gsr.gsr_interface) != 1) {
+    LOG(ERROR) << "Malformed IP interface address "
+               << options->interface_address();
+    return false;
+  }
+  if (inet_pton(ai_family,
+                options->source_address().c_str(),
+                &gsr.gsr_source) != 1) {
+    LOG(ERROR) << "Malformed IP source specific multicast address "
+               << options->source_address();
+    return false;
+  }
+  if (setsockopt(socket,
+                 level,
+                 MCAST_JOIN_SOURCE_GROUP,
+                 reinterpret_cast<const char*>(&gsr),
+                 sizeof(gsr)) < 0) {
+    LOG(ERROR) << "Failed to join multicast group.";
+    return false;
+  }
+
+  return true;
+}
+
+bool Multicast(int ai_family,
+               int level,
+               SOCKET socket,
+               struct group_req& greq,
+               std::unique_ptr<UdpOptions>&& options) {
+    if (inet_pton(ai_family,
+                  options->interface_address().c_str(),
+                  &greq.gr_interface) != 1) {
+      LOG(ERROR) << "Malformed IP interface address "
+                 << options->interface_address();
+      return false;
+    }
+    if (setsockopt(socket, level, MCAST_JOIN_GROUP,
+                  reinterpret_cast<const char*>(&greq),
+                  sizeof(greq)) < 0) {
+      LOG(ERROR) << "Failed to join multicast group.";
+      return false;
+    }
+
+#if defined(__linux__)
+    // Disable IP_MULTICAST_ALL to avoid interference caused when two sockets
+    // are bound to the same port but joined to different multicast groups.
+    const int optval_zero = 0;
+    if (setsockopt(socket, level, IP_MULTICAST_ALL,
+                   reinterpret_cast<const char*>(&optval_zero),
+                   sizeof(optval_zero)) < 0 &&
+        errno != ENOPROTOOPT) {
+      LOG(ERROR) << "Failed to disable IP_MULTICAST_ALL option.";
+      return false;
+    }
+#endif  // #if defined(__linux__)
+
+    return true;
 }
 
 }  // anonymous namespace
@@ -163,27 +241,26 @@ bool UdpFile::Open() {
   if (!options)
     return false;
 
-  ScopedSocket new_socket(socket(AF_INET, SOCK_DGRAM, 0));
+  unsigned long addr = inet_addr(options->address().c_str());
+
+  if (addr == INADDR_NONE)
+    return false;
+
+  struct addrinfo hints = {0};
+  struct hostent *host_info = gethostbyaddr((const char *)&addr, (socklen_t)sizeof(SOCKET), AF_UNSPEC);
+  struct addrinfo *res;
+
+  hints.ai_family = AF_UNSPEC;
+  hints.ai_socktype = SOCK_DGRAM;
+
+  if (getaddrinfo(host_info->h_name, std::to_string(options->port()).c_str(), &hints, &res) != 0) {
+    LOG(ERROR) << "getaddrinfo";
+    return false;
+  }
+  ScopedSocket new_socket(socket(res->ai_family, res->ai_socktype, res->ai_protocol));
   if (new_socket.get() == INVALID_SOCKET) {
     LOG(ERROR) << "Could not allocate socket.";
     return false;
-  }
-
-  struct in_addr local_in_addr = {0};
-  if (inet_pton(AF_INET, options->address().c_str(), &local_in_addr) != 1) {
-    LOG(ERROR) << "Malformed IPv4 address " << options->address();
-    return false;
-  }
-
-  struct sockaddr_in local_sock_addr = {0};
-  // TODO(kqyang): Support IPv6.
-  local_sock_addr.sin_family = AF_INET;
-  local_sock_addr.sin_port = htons(options->port());
-  const bool is_multicast = IsIpv4MulticastAddress(local_in_addr);
-  if (is_multicast) {
-    local_sock_addr.sin_addr.s_addr = htonl(INADDR_ANY);
-  } else {
-    local_sock_addr.sin_addr = local_in_addr;
   }
 
   if (options->reuse()) {
@@ -197,75 +274,100 @@ bool UdpFile::Open() {
     }
   }
 
+  // Use `sizeof(&res->ai_addr)` instead of `res->ai_addrlen` (for x64)
   if (bind(new_socket.get(),
-           reinterpret_cast<struct sockaddr*>(&local_sock_addr),
-           sizeof(local_sock_addr))) {
+           reinterpret_cast<struct sockaddr*>(res->ai_addr),
+           sizeof(&res->ai_addr)) < 0) {
     LOG(ERROR) << "Could not bind UDP socket";
     return false;
   }
 
-  if (is_multicast) {
-    if (options->is_source_specific_multicast()) {
-      struct ip_mreq_source source_multicast_group;
-
-      source_multicast_group.imr_multiaddr = local_in_addr;
-      if (inet_pton(AF_INET,
-                    options->interface_address().c_str(),
-                    &source_multicast_group.imr_interface) != 1) {
-        LOG(ERROR) << "Malformed IPv4 interface address "
-                   << options->interface_address();
+  switch (res->ai_family) {
+    case AF_INET: {
+      struct in_addr local_in_addr = {0};
+      if (inet_pton(AF_INET, options->address().c_str(), &local_in_addr) != 1) {
+        LOG(ERROR) << "Malformed IPv4 address " << options->address();
         return false;
       }
-      if (inet_pton(AF_INET,
-                    options->source_address().c_str(),
-                    &source_multicast_group.imr_sourceaddr) != 1) {
-        LOG(ERROR) << "Malformed IPv4 source specific multicast address "
-                   << options->source_address();
-        return false;
+      const bool is_multicast = IsIpv4MulticastAddress(local_in_addr);
+      if (is_multicast) {
+        if (options->is_source_specific_multicast()) {
+          struct group_source_req gsr;
+          gsr.gsr_interface = 0;
+          memcpy(&gsr.gsr_group, res->ai_addr, res->ai_addrlen);
+          memcpy(&gsr.gsr_source, res->ai_addr, res->ai_addrlen);
+
+          bool result = SpecificMalticast(AF_INET,
+                                          IPPROTO_IP,
+                                          new_socket.get(),
+                                          gsr,
+                                          std::move(options));
+          if (!result) {
+            return false;
+          }
+        } else {
+          // this is a v2 join without a specific source.
+          struct group_req greq;
+          greq.gr_interface = 0;
+          memcpy(&greq.gr_group, res->ai_addr, res->ai_addrlen);
+
+          bool result = Multicast(AF_INET,
+                                  IPPROTO_IP,
+                                  new_socket.get(),
+                                  greq,
+                                  std::move(options));
+          if (!result) {
+            return false;
+          }
+        }
       }
-
-      if (setsockopt(new_socket.get(),
-                     IPPROTO_IP,
-                     IP_ADD_SOURCE_MEMBERSHIP,
-                     reinterpret_cast<const char*>(&source_multicast_group),
-                     sizeof(source_multicast_group)) < 0) {
-          LOG(ERROR) << "Failed to join multicast group.";
-          return false;
-      }
-    } else {
-      // this is a v2 join without a specific source.
-      struct ip_mreq multicast_group;
-
-      multicast_group.imr_multiaddr = local_in_addr;
-
-      if (inet_pton(AF_INET, options->interface_address().c_str(),
-                    &multicast_group.imr_interface) != 1) {
-        LOG(ERROR) << "Malformed IPv4 interface address "
-                   << options->interface_address();
-        return false;
-      }
-
-      if (setsockopt(new_socket.get(), IPPROTO_IP, IP_ADD_MEMBERSHIP,
-                    reinterpret_cast<const char*>(&multicast_group),
-                    sizeof(multicast_group)) < 0) {
-        LOG(ERROR) << "Failed to join multicast group.";
-        return false;
-      }
-
-  }
-
-#if defined(__linux__)
-    // Disable IP_MULTICAST_ALL to avoid interference caused when two sockets
-    // are bound to the same port but joined to different multicast groups.
-    const int optval_zero = 0;
-    if (setsockopt(new_socket.get(), IPPROTO_IP, IP_MULTICAST_ALL,
-                   reinterpret_cast<const char*>(&optval_zero),
-                   sizeof(optval_zero)) < 0 &&
-        errno != ENOPROTOOPT) {
-      LOG(ERROR) << "Failed to disable IP_MULTICAST_ALL option.";
-      return false;
+      break;
     }
-#endif  // #if defined(__linux__)
+    case AF_INET6: {
+      struct in6_addr local_in_addr = {{}};
+      if (inet_pton(AF_INET6, options->address().c_str(), &local_in_addr) != 1) {
+        LOG(ERROR) << "Malformed IPv6 address " << options->address();
+        freeaddrinfo(res);
+        return false;
+      }
+      const bool is_multicast = IsIpv6MulticastAddress(local_in_addr);
+      if (is_multicast) {
+        if (options->is_source_specific_multicast()) {
+          struct group_source_req gsr;
+          gsr.gsr_interface = 0;
+          memcpy(&gsr.gsr_group, res->ai_addr, res->ai_addrlen);
+          memcpy(&gsr.gsr_source, res->ai_addr, res->ai_addrlen);
+
+          bool result = SpecificMalticast(AF_INET6,
+                                          IPPROTO_IPV6,
+                                          new_socket.get(),
+                                          gsr,
+                                          std::move(options));
+          if (!result) {
+            return false;
+          }
+        } else {
+          // this is a v2 join without a specific source.
+          struct group_req greq;
+          greq.gr_interface = 0;
+          memcpy(&greq.gr_group, res->ai_addr, res->ai_addrlen);
+
+          bool result = Multicast(AF_INET6,
+                                  IPPROTO_IPV6,
+                                  new_socket.get(),
+                                  greq,
+                                  std::move(options));
+          if (!result) {
+            return false;
+          }
+        }
+      }
+      break;
+    }
+    default:
+      assert(false);
+      LOG(ERROR) << "Unknown `ai_family`";
+      break;
   }
 
   // Set timeout if needed.
@@ -292,6 +394,9 @@ bool UdpFile::Open() {
   }
 
   socket_ = new_socket.release();
+
+  freeaddrinfo(res);
+
   return true;
 }
 
