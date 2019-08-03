edd776eea2a79c3d6aafc4b5858ce1d200b814e6
KongQun Yang <kqyang@google.com>
2019-08-02 17:19:13 -0700

    Move DerivePixelWidthHeight to video_util.cc
    Change-Id: I14df8b39fb58078635f08377f4177a45e341be79

--- packager/media/formats/mp4/mp4_media_parser.cc
+++ packager/media/formats/mp4/mp4_media_parser.cc
@@ -5,7 +5,6 @@
 #include "packager/media/formats/mp4/mp4_media_parser.h"
 
 #include <algorithm>
-#include <limits>
 
 #include "packager/base/callback.h"
 #include "packager/base/callback_helpers.h"
@@ -21,6 +20,7 @@
 #include "packager/media/base/media_sample.h"
 #include "packager/media/base/rcheck.h"
 #include "packager/media/base/video_stream_info.h"
+#include "packager/media/base/video_util.h"
 #include "packager/media/codecs/ac3_audio_util.h"
 #include "packager/media/codecs/av1_codec_configuration_record.h"
 #include "packager/media/codecs/avc_decoder_configuration_record.h"
@@ -113,56 +113,6 @@ Codec ObjectTypeToCodec(ObjectType object_type) {
     default:
       return kUnknownCodec;
   }
-}
-
-uint64_t CalculateGCD(uint64_t a, uint64_t b) {
-  while (b != 0) {
-    uint64_t temp = a;
-    a = b;
-    b = temp % b;
-  }
-  return a;
-}
-
-void ReducePixelWidthHeight(uint64_t* pixel_width, uint64_t* pixel_height) {
-  if (*pixel_width == 0 || *pixel_height == 0)
-    return;
-  const uint64_t kMaxUint32 = std::numeric_limits<uint32_t>::max();
-  while (true) {
-    uint64_t gcd = CalculateGCD(*pixel_width, *pixel_height);
-    *pixel_width /= gcd;
-    *pixel_height /= gcd;
-    // Both width and height needs to be 32 bit or less.
-    if (*pixel_width <= kMaxUint32 && *pixel_height <= kMaxUint32)
-      break;
-    *pixel_width >>= 1;
-    *pixel_height >>= 1;
-  }
-}
-
-// Derive pixel aspect ratio from Display Aspect Ratio and Frame Aspect Ratio.
-//   DAR = PAR * FAR => PAR = DAR / FAR.
-//   Thus:
-//     pixel_width             display_width            frame_width
-//     -----------      =      -------------      /     -----------
-//     pixel_height            display_height           frame_height
-//   So:
-//     pixel_width             display_width  x  frame_width
-//     -----------      =      ------------------------------
-//     pixel_height            display_height x  frame_height
-void DerivePixelWidthHeight(uint32_t frame_width,
-                            uint32_t frame_height,
-                            uint32_t display_width,
-                            uint32_t display_height,
-                            uint32_t* pixel_width,
-                            uint32_t* pixel_height) {
-  uint64_t pixel_width_unreduced =
-      static_cast<uint64_t>(display_width) * frame_height;
-  uint64_t pixel_height_unreduced =
-      static_cast<uint64_t>(display_height) * frame_width;
-  ReducePixelWidthHeight(&pixel_width_unreduced, &pixel_height_unreduced);
-  *pixel_width = pixel_width_unreduced;
-  *pixel_height = pixel_height_unreduced;
 }
 
 const uint64_t kNanosecondsPerSecond = 1000000000ull;
