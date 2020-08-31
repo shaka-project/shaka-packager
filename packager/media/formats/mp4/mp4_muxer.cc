5c3e5d39f91a1f4552aa1ff01343f3e395149d71
Anthony Lu <koln@google.com>
2020-08-31 19:10:02 +0000

    mp41 major brand

--- packager/media/formats/mp4/mp4_muxer.cc
+++ packager/media/formats/mp4/mp4_muxer.cc
@@ -206,11 +206,10 @@ Status MP4Muxer::DelayInitializeMuxer() {
   std::unique_ptr<FileType> ftyp(new FileType);
   std::unique_ptr<Movie> moov(new Movie);
 
-  ftyp->major_brand = FOURCC_avc1;
+  ftyp->major_brand = FOURCC_mp41;
   ftyp->compatible_brands.push_back(FOURCC_iso8);
   ftyp->compatible_brands.push_back(FOURCC_mp41);
   ftyp->compatible_brands.push_back(FOURCC_dash);
-  ftyp->compatible_brands.push_back(FOURCC_avc1);
 
   if (streams().size() == 1) {
     FourCC codec_fourcc = FOURCC_NULL;
