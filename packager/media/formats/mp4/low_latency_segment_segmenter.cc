8d0d908cabf32bcf912e0eb16f6f46d9a52d9c24
Caitlin O'Callaghan <cocallaghan@fb.com>
2021-08-03 15:56:56 -0700

    Remove unnecessary log

--- packager/media/formats/mp4/low_latency_segment_segmenter.cc
+++ packager/media/formats/mp4/low_latency_segment_segmenter.cc
@@ -178,7 +178,6 @@ Status LowLatencySegmentSegmenter::WriteChunk() {
 
 Status LowLatencySegmentSegmenter::FinalizeSegment() {
   // Close the file now that the final chunk has been written
-  LOG(INFO) << "finalizing segment";
   if (!segment_file_.release()->Close()) {
     return Status(
         error::FILE_FAILURE,
