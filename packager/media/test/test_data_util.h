d7d600527b97d08bead59e9c1c9336dad0114c15
Joey Parrish <joeyparrish@google.com>
2022-10-24 09:16:32 -0700

    feat: port media/test
    Rewrite test_data_util.h to locate files relative to the source file
    itself, rather than using a service from chromium base::.  This makes
    the functionality header-only, but there is no meaningful downside to
    inlining complex functions for tests.

--- packager/media/test/test_data_util.h
+++ packager/media/test/test_data_util.h
@@ -7,21 +7,46 @@
 
 #include <stdint.h>
 
+#include <filesystem>
 #include <string>
 
-#include "packager/base/files/file_path.h"
+#include "glog/logging.h"
 
 namespace shaka {
 namespace media {
 
 // Returns a file path for a file in the media/test/data directory.
-base::FilePath GetTestDataFilePath(const std::string& name);
+std::filesystem::path GetTestDataFilePath(const std::string& name) {
+  std::filesystem::path header_path(__FILE__);
+  return header_path.parent_path() / "data" / name;
+}
 
 // Returns a file path for a file in the media/app/test/testdata directory.
-base::FilePath GetAppTestDataFilePath(const std::string& name);
+std::filesystem::path GetAppTestDataFilePath(const std::string& name) {
+  std::filesystem::path header_path(__FILE__);
+  return header_path.parent_path().parent_path() / "app" / "test" / "testdata"
+                                                 / name;
+}
 
 // Reads a test file from media/test/data directory and returns its content.
-std::vector<uint8_t> ReadTestDataFile(const std::string& name);
+std::vector<uint8_t> ReadTestDataFile(const std::string& name) {
+  std::filesystem::path path = GetTestDataFilePath(name);
+
+  std::vector<uint8_t> data;
+  data.resize(std::filesystem::file_size(path));
+
+  FILE* f = fopen(path.string().c_str(), "rb");
+  if (!f) {
+    LOG(FATAL) << "Failed to read test data from " << path;
+    return std::vector<uint8_t>();
+  }
+
+  size_t size = fread(data.data(), 1, data.size(), f);
+  data.resize(size);
+  fclose(f);
+
+  return data;
+}
 
 }  // namespace media
 }  // namespace shaka
