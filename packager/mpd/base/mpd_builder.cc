bd832d2d1dd04c3eccd7550380f0a967f5137422
Sergio Garcia Murillo <sergio.garcia.murillo@gmail.com>
2023-07-12 20:20:44 +0200

    remove trailing spaces

--- packager/mpd/base/mpd_builder.cc
+++ packager/mpd/base/mpd_builder.cc
@@ -343,7 +343,7 @@ bool MpdBuilder::GetEarliestTimestamp(double* timestamp_seconds) {
   DCHECK(timestamp_seconds);
   DCHECK(!periods_.empty());
   if (periods_.empty())
-    return false;    
+    return false;
   double timestamp = 0;
   double earliest_timestamp = -1;
   // TODO(kqyang): This is used to set availabilityStartTime. We may consider
