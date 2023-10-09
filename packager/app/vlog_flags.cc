dee3d7bd82190bdcfe87f155fbe6bc559c75d278
Joey Parrish <joeyparrish@google.com>
2023-10-09 10:22:49 -0700

    Fix backward compatibility

--- packager/app/vlog_flags.cc
+++ packager/app/vlog_flags.cc
@@ -7,20 +7,18 @@
 // Defines verbose logging flags.
 
 #include "packager/app/vlog_flags.h"
-#include <glog/logging.h>
-#include <glog/vlog_is_on.h>
 #include "absl/strings/numbers.h"
 #include "packager/kv_pairs/kv_pairs.h"
 
 ABSL_FLAG(int32_t,
-          vlog,
+          v,
           0,
           "Show all VLOG(m) or DVLOG(m) messages for m <= this. "
-          "Overridable by --vlog_modules.");
+          "Overridable by --vmodule.");
 
 ABSL_FLAG(
     std::string,
-    vlog_modules,
+    vmodule,
     "",
     "Per-module verbose level."
     "Argument is a comma-separated list of <module name>=<log level>. "
@@ -30,17 +28,27 @@ ABSL_FLAG(
     "the whole file path (still without .cc/.h./-inl.h) is matched. "
     "? and * in the glob pattern match any single or sequence of characters "
     "respectively including slashes. "
-    "<log level> overrides any value given by --vlog.");
+    "<log level> overrides any value given by --v.");
+
+// logging.h defines FLAGS_v and FLAGS_vmodule in terms of the gflags library,
+// which we do not use.  Here we use macros to rename those symbols to avoid a
+// conflict with the flags we defined above using absl.  When we switch from
+// glog to absl::logging, this workaround should be removed.
+#define FLAGS_v GLOG_FLAGS_v
+#define FLAGS_vmodule GLOG_FLAGS_vmodule
+#include <glog/logging.h>
+#undef FLAGS_vmodule
+#undef FLAGS_v
 
 namespace shaka {
 
 void register_flags_with_glog() {
-  auto vlog_level = absl::GetFlag(FLAGS_vlog);
+  auto vlog_level = absl::GetFlag(FLAGS_v);
   if (vlog_level != 0) {
     google::SetVLOGLevel("*", vlog_level);
   }
 
-  std::string vmodule_patterns = absl::GetFlag(FLAGS_vlog_modules);
+  std::string vmodule_patterns = absl::GetFlag(FLAGS_vmodule);
   if (!vmodule_patterns.empty()) {
     std::vector<KVPair> patterns =
         SplitStringIntoKeyValuePairs(vmodule_patterns, '=', ',');
@@ -48,7 +56,7 @@ void register_flags_with_glog() {
 
     for (const auto& pattern : patterns) {
       if (!::absl::SimpleAtoi(pattern.second, &pattern_vlevel)) {
-        LOG(ERROR) << "Error parsing vlog level for '" << pattern.first
+        LOG(ERROR) << "Error parsing log level for '" << pattern.first
                    << "' from '" << pattern.second << "'";
         continue;
       }
