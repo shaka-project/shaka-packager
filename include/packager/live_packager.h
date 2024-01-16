4e80a1caa300959ba56b605b5f0f7f0364ec7790
Oleksandr Khodos <oleksandr.khodos@pluto.tv>
2024-01-16 16:38:46 +0200

    Fix typo

--- include/packager/live_packager.h
+++ include/packager/live_packager.h
@@ -78,7 +78,7 @@ struct LiveConfig {
 
   OutputFormat format;
   TrackType track_type;
-  // TOOD: do we need non-integer durations?
+  // TODO(david): do we need non-integer durations?
   double segment_duration_sec = 5.0;
 
   // TODO: should we allow for keys to be hex string?
