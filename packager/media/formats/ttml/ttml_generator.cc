137fe6946a340c781911a71ee27c8dc12b37e40e
Torbjörn Einarson <torbjorn@einarssons.se>
2024-03-22 11:01:56 +0100

    feat: ttml generation for teletext to EBU-TT-D
    Add support to render teletext input in EBU-TT-D (IMSC-1) format.
    This includes appropriate regions ttx_0 to ttx_11 signalled
    in the TextSamples, alignment and text and background colors.
    
    The general TTML output has been changed to always include
    metadata, layout, and styling nodes, even if they are empty.
    
    EBU-TT-D is detected by the presence of "ttx_?" regions in the
    samples. If detected, extra TTML elements will be added and
    the EBU-TT-D linePadding used as well.
    
    Appropriate styles for background and text colors are generated
    depending on the color and backgroundColor attributes in the
    text fragments.

--- packager/media/formats/ttml/ttml_generator.cc
+++ packager/media/formats/ttml/ttml_generator.cc
@@ -17,6 +17,7 @@ namespace ttml {
 namespace {
 
 constexpr const char* kRegionIdPrefix = "_shaka_region_";
+constexpr const char* kRegionTeletextPrefix = "ttx_";
 
 std::string ToTtmlTime(int64_t time, int32_t timescale) {
   int64_t remaining = time * 1000 / timescale;
@@ -53,6 +54,18 @@ void TtmlGenerator::Initialize(const std::map<std::string, TextRegion>& regions,
   regions_ = regions;
   language_ = language;
   time_scale_ = time_scale;
+  // Add ebu_tt_d_regions
+  float step = 74.1 / 11;
+  for (int i = 0; i < 12; i++) {
+    TextRegion region;
+    float verPos = 10.0 + int(float(step) * float(i));
+    region.width = TextNumber(80, TextUnitType::kPercent);
+    region.height = TextNumber(15, TextUnitType::kPercent);
+    region.window_anchor_x = TextNumber(10, TextUnitType::kPercent);
+    region.window_anchor_y = TextNumber(verPos, TextUnitType::kPercent);
+    const std::string id = kRegionTeletextPrefix + std::to_string(i);
+    regions_.emplace(id, region);
+  }
 }
 
 void TtmlGenerator::AddSample(const TextSample& sample) {
@@ -65,72 +78,92 @@ void TtmlGenerator::Reset() {
 
 bool TtmlGenerator::Dump(std::string* result) const {
   xml::XmlNode root("tt");
+  bool ebuTTDFormat = isEbuTTTD();
   RCHECK(root.SetStringAttribute("xmlns", kTtNamespace));
   RCHECK(root.SetStringAttribute("xmlns:tts",
                                  "http://www.w3.org/ns/ttml#styling"));
-
-  bool did_log = false;
-  xml::XmlNode head("head");
+  RCHECK(root.SetStringAttribute("xmlns:tts",
+                                 "http://www.w3.org/ns/ttml#styling"));
   RCHECK(root.SetStringAttribute("xml:lang", language_));
-  for (const auto& pair : regions_) {
-    if (!did_log && (pair.second.region_anchor_x.value != 0 &&
-                     pair.second.region_anchor_y.value != 0)) {
-      LOG(WARNING) << "TTML doesn't support non-0 region anchor";
-      did_log = true;
-    }
 
-    xml::XmlNode region("region");
-    const auto origin =
-        ToTtmlSize(pair.second.window_anchor_x, pair.second.window_anchor_y);
-    const auto extent = ToTtmlSize(pair.second.width, pair.second.height);
-    RCHECK(region.SetStringAttribute("xml:id", pair.first));
-    RCHECK(region.SetStringAttribute("tts:origin", origin));
-    RCHECK(region.SetStringAttribute("tts:extent", extent));
-    RCHECK(head.AddChild(std::move(region)));
+  if (ebuTTDFormat) {
+    RCHECK(root.SetStringAttribute("xmlns:ttp",
+                                   "http://www.w3.org/ns/ttml#parameter"));
+    RCHECK(root.SetStringAttribute("xmlns:ttm",
+                                   "http://www.w3.org/ns/ttml#metadata"));
+    RCHECK(root.SetStringAttribute("xmlns:ebuttm", "urn:ebu:tt:metadata"));
+    RCHECK(root.SetStringAttribute("xmlns:ebutts", "urn:ebu:tt:style"));
+    RCHECK(root.SetStringAttribute("xml:space", "default"));
+    RCHECK(root.SetStringAttribute("ttp:timeBase", "media"));
+    RCHECK(root.SetStringAttribute("ttp:cellResolution", "32 15"));
   }
-  RCHECK(root.AddChild(std::move(head)));
 
-  size_t image_count = 0;
+  xml::XmlNode head("head");
+  xml::XmlNode styling("styling");
   xml::XmlNode metadata("metadata");
+  xml::XmlNode layout("layout");
+  RCHECK(addRegions(layout));
+
   xml::XmlNode body("body");
+  if (ebuTTDFormat) {
+    RCHECK(body.SetStringAttribute("style", "default"));
+  }
+  size_t image_count = 0;
+  std::unordered_set<std::string> fragmentStyles;
   xml::XmlNode div("div");
   for (const auto& sample : samples_) {
-    RCHECK(AddSampleToXml(sample, &div, &metadata, &image_count));
+    RCHECK(
+        AddSampleToXml(sample, &div, &metadata, fragmentStyles, &image_count));
   }
-  RCHECK(body.AddChild(std::move(div)));
   if (image_count > 0) {
     RCHECK(root.SetStringAttribute(
         "xmlns:smpte", "http://www.smpte-ra.org/schemas/2052-1/2010/smpte-tt"));
-    RCHECK(root.AddChild(std::move(metadata)));
   }
+  RCHECK(body.AddChild(std::move(div)));
+  RCHECK(head.AddChild(std::move(metadata)));
+  RCHECK(addStyling(styling, fragmentStyles));
+  RCHECK(head.AddChild(std::move(styling)));
+  RCHECK(head.AddChild(std::move(layout)));
+  RCHECK(root.AddChild(std::move(head)));
+
   RCHECK(root.AddChild(std::move(body)));
 
   *result = root.ToString(/* comment= */ "");
   return true;
 }
 
-bool TtmlGenerator::AddSampleToXml(const TextSample& sample,
-                                   xml::XmlNode* body,
-                                   xml::XmlNode* metadata,
-                                   size_t* image_count) const {
+bool TtmlGenerator::AddSampleToXml(
+    const TextSample& sample,
+    xml::XmlNode* body,
+    xml::XmlNode* metadata,
+    std::unordered_set<std::string>& fragmentStyles,
+    size_t* image_count) const {
   xml::XmlNode p("p");
-  RCHECK(p.SetStringAttribute("xml:space", "preserve"));
+  if (!isEbuTTTD()) {
+    RCHECK(p.SetStringAttribute("xml:space", "preserve"));
+  }
   RCHECK(p.SetStringAttribute("begin",
                               ToTtmlTime(sample.start_time(), time_scale_)));
   RCHECK(
       p.SetStringAttribute("end", ToTtmlTime(sample.EndTime(), time_scale_)));
-  RCHECK(ConvertFragmentToXml(sample.body(), &p, metadata, image_count));
+  RCHECK(ConvertFragmentToXml(sample.body(), &p, metadata, fragmentStyles,
+                              image_count));
   if (!sample.id().empty())
     RCHECK(p.SetStringAttribute("xml:id", sample.id()));
 
   const auto& settings = sample.settings();
-  if (settings.line || settings.position || settings.width || settings.height) {
-    // TTML positioning needs to be from a region.
-    if (!settings.region.empty()) {
-      LOG(WARNING)
-          << "Using both text regions and positioning isn't supported in TTML";
+  bool regionFound = false;
+  if (!settings.region.empty()) {
+    auto reg = regions_.find(settings.region);
+    if (reg != regions_.end()) {
+      regionFound = true;
+      RCHECK(p.SetStringAttribute("region", settings.region));
     }
+  }
 
+  if (!regionFound && (settings.line || settings.position || settings.width ||
+                       settings.height)) {
+    // TTML positioning needs to be from a region.
     const auto origin = ToTtmlSize(
         settings.position.value_or(TextNumber(0, TextUnitType::kPixels)),
         settings.line.value_or(TextNumber(0, TextUnitType::kPixels)));
@@ -145,8 +178,6 @@ bool TtmlGenerator::AddSampleToXml(const TextSample& sample,
     RCHECK(region.SetStringAttribute("tts:extent", extent));
     RCHECK(p.SetStringAttribute("region", id));
     RCHECK(body->AddChild(std::move(region)));
-  } else if (!settings.region.empty()) {
-    RCHECK(p.SetStringAttribute("region", settings.region));
   }
 
   if (settings.writing_direction != WritingDirection::kHorizontal) {
@@ -178,19 +209,22 @@ bool TtmlGenerator::AddSampleToXml(const TextSample& sample,
   return true;
 }
 
-bool TtmlGenerator::ConvertFragmentToXml(const TextFragment& body,
-                                         xml::XmlNode* parent,
-                                         xml::XmlNode* metadata,
-                                         size_t* image_count) const {
+bool TtmlGenerator::ConvertFragmentToXml(
+    const TextFragment& body,
+    xml::XmlNode* parent,
+    xml::XmlNode* metadata,
+    std::unordered_set<std::string>& fragmentStyles,
+    size_t* image_count) const {
   if (body.newline) {
     xml::XmlNode br("br");
     return parent->AddChild(std::move(br));
   }
-
-  // If we have new styles, add a new <span>.
   xml::XmlNode span("span");
   xml::XmlNode* node = parent;
-  if (body.style.bold || body.style.italic || body.style.underline) {
+  bool useSpan =
+      (body.style.bold || body.style.italic || body.style.underline ||
+       !body.style.color.empty() || !body.style.backgroundColor.empty());
+  if (useSpan) {
     node = &span;
     if (body.style.bold) {
       RCHECK(span.SetStringAttribute("tts:fontWeight",
@@ -205,6 +239,20 @@ bool TtmlGenerator::ConvertFragmentToXml(const TextFragment& body,
           "tts:textDecoration",
           *body.style.underline ? "underline" : "noUnderline"));
     }
+    std::string color = "white";
+    std::string backgroundColor = "black";
+
+    if (!body.style.color.empty()) {
+      color = body.style.color;
+    }
+
+    if (!body.style.backgroundColor.empty()) {
+      backgroundColor = body.style.backgroundColor;
+    }
+
+    const std::string fragStyle = color + "_" + backgroundColor;
+    fragmentStyles.insert(fragStyle);
+    RCHECK(span.SetStringAttribute("style", fragStyle));
   }
 
   if (!body.body.empty()) {
@@ -225,14 +273,89 @@ bool TtmlGenerator::ConvertFragmentToXml(const TextFragment& body,
     RCHECK(node->SetStringAttribute("smpte:backgroundImage", "#" + id));
   } else {
     for (const auto& frag : body.sub_fragments) {
-      if (!ConvertFragmentToXml(frag, node, metadata, image_count))
+      if (!ConvertFragmentToXml(frag, node, metadata, fragmentStyles,
+                                image_count))
         return false;
     }
   }
 
-  if (body.style.bold || body.style.italic || body.style.underline)
+  if (useSpan)
     RCHECK(parent->AddChild(std::move(span)));
   return true;
+}
+
+std::vector<std::string> TtmlGenerator::usedRegions() const {
+  std::vector<std::string> uRegions;
+  for (const auto& sample : samples_) {
+    if (!sample.settings().region.empty()) {
+      uRegions.push_back(sample.settings().region);
+    }
+  }
+  return uRegions;
+}
+
+bool TtmlGenerator::addRegions(xml::XmlNode& layout) const {
+  auto regNames = usedRegions();
+  for (const auto& r : regions_) {
+    bool used = false;
+    for (const auto& name : regNames) {
+      if (r.first == name) {
+        used = true;
+      }
+    }
+    if (used) {
+      xml::XmlNode region("region");
+      const auto origin =
+          ToTtmlSize(r.second.window_anchor_x, r.second.window_anchor_y);
+      const auto extent = ToTtmlSize(r.second.width, r.second.height);
+      RCHECK(region.SetStringAttribute("xml:id", r.first));
+      RCHECK(region.SetStringAttribute("tts:origin", origin));
+      RCHECK(region.SetStringAttribute("tts:extent", extent));
+      RCHECK(region.SetStringAttribute("tts:overflow", "visible"));
+      RCHECK(layout.AddChild(std::move(region)));
+    }
+  }
+  return true;
+}
+
+bool TtmlGenerator::addStyling(
+    xml::XmlNode& styling,
+    const std::unordered_set<std::string>& fragmentStyles) const {
+  if (fragmentStyles.empty()) {
+    return true;
+  }
+  // Add default style
+  xml::XmlNode defaultStyle("style");
+  RCHECK(defaultStyle.SetStringAttribute("xml:id", "default"));
+  RCHECK(defaultStyle.SetStringAttribute("tts:fontStyle", "normal"));
+  RCHECK(defaultStyle.SetStringAttribute("tts:fontFamily", "sansSerif"));
+  RCHECK(defaultStyle.SetStringAttribute("tts:fontSize", "100%"));
+  RCHECK(defaultStyle.SetStringAttribute("tts:lineHeight", "normal"));
+  RCHECK(defaultStyle.SetStringAttribute("tts:textAlign", "center"));
+  RCHECK(defaultStyle.SetStringAttribute("ebutts:linePadding", "0.5c"));
+  RCHECK(styling.AddChild(std::move(defaultStyle)));
+
+  for (const auto& name : fragmentStyles) {
+    auto pos = name.find('_');
+    auto color = name.substr(0, pos);
+    auto backgroundColor = name.substr(pos + 1, name.size());
+    xml::XmlNode fragStyle("style");
+    RCHECK(fragStyle.SetStringAttribute("xml:id", name));
+    RCHECK(
+        fragStyle.SetStringAttribute("tts:backgroundColor", backgroundColor));
+    RCHECK(fragStyle.SetStringAttribute("tts:color", color));
+    RCHECK(styling.AddChild(std::move(fragStyle)));
+  }
+  return true;
+}
+
+bool TtmlGenerator::isEbuTTTD() const {
+  for (const auto& sample : samples_) {
+    if (sample.settings().region.rfind(kRegionTeletextPrefix, 0) == 0) {
+      return true;
+    }
+  }
+  return false;
 }
 
 }  // namespace ttml
