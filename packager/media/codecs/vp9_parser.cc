5e71a84be8b3989b6dfc039f97397c3c8880b508
Cosmin Stejerean <cstejerean@meta.com>
2024-05-09 15:28:19 -0700

    fix: set yuv full range flag to 1 for VP9 with sRGB
    Fixes #990

--- packager/media/codecs/vp9_parser.cc
+++ packager/media/codecs/vp9_parser.cc
@@ -285,6 +285,7 @@ bool ReadBitDepthAndColorSpace(BitReader* reader,
     }
   } else {
     // Assume 4:4:4 for colorspace SRGB.
+    yuv_full_range = 1;
     chroma_subsampling = VPCodecConfigurationRecord::CHROMA_444;
     if (codec_config->profile() & 1) {
       bool reserved;
