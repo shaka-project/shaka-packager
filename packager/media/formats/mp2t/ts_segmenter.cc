9bdc8aa9fe5b6c9892fd102bbbfe9d70f7d22ae6
Igor Dagayev <igor.dagayev@pluto.tv>
2025-03-03 16:32:25 -0500

    fix constructor

--- packager/media/formats/mp2t/ts_segmenter.cc
+++ packager/media/formats/mp2t/ts_segmenter.cc
@@ -38,7 +38,8 @@ bool IsVideoCodec(Codec codec) {
 }  // namespace
 
 TsSegmenter::TsSegmenter(const MuxerOptions& options, MuxerListener* listener)
-    : listener_(listener),
+    : muxer_options_(options),
+      listener_(listener),
       transport_stream_timestamp_offset_(
           options.transport_stream_timestamp_offset_ms * kTsTimescale / 1000),
       pes_packet_generator_(
