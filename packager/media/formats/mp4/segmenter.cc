0c96d5a16093145e360405b5b75fb6f6cda79e56
Igor Dagayev <igor.dagayev@pluto.tv>
2025-03-10 15:21:34 -0400

    move include

--- packager/media/formats/mp4/segmenter.cc
+++ packager/media/formats/mp4/segmenter.cc
@@ -4,12 +4,12 @@
 // license that can be found in the LICENSE file or at
 // https://developers.google.com/open-source/licenses/bsd
 
-#include <packager/media/formats/mp4/segmenter.h>
-
 #include <algorithm>
 
 #include <absl/log/check.h>
 #include <absl/log/log.h>
+
+#include <packager/media/formats/mp4/segmenter.h>
 
 #include <packager/media/base/buffer_writer.h>
 #include <packager/media/base/id3_tag.h>
