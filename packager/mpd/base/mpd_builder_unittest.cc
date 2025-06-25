d3ad82e5f446faedb4d2307205993abb9fd1ad1c
Igor Dagayev <igor.dagayev@pluto.tv>
2025-06-25 11:16:58 -0400

    remove test changes

--- packager/mpd/base/mpd_builder_unittest.cc
+++ packager/mpd/base/mpd_builder_unittest.cc
@@ -60,8 +60,6 @@ class MpdBuilderTest : public ::testing::Test {
 
     AdaptationSet* adaptation_set =
         period->GetOrCreateAdaptationSet(media_info, kContentProtectionFlag);
-    if (!adaptation_set->has_id())
-      adaptation_set->set_id(next_adaptation_set_id_++);
     Representation* representation =
         adaptation_set->AddRepresentation(media_info);
     representation->AddNewSegment(
@@ -92,7 +90,6 @@ class MpdBuilderTest : public ::testing::Test {
 
  private:
   Period* period_ = nullptr;
-  uint32_t next_adaptation_set_id_ = 0;
 
   DISALLOW_COPY_AND_ASSIGN(MpdBuilderTest);
 };
