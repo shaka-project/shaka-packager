4fad8d01c0bdac3b927195ccd2aeaaf882c6a61a
SteveR-PMP <122415851+SteveR-PMP@users.noreply.github.com>
2026-03-04 14:48:15 -0800

    feat: --local_targetduration (#1519)
    Change EXT-X-TARGETDURATION to have the value set to the highest
    duration for each variant instead of the highest duration for all the
    variants.
    
    Fixes #1510

--- packager/app/manifest_flags.h
+++ packager/app/manifest_flags.h
@@ -19,5 +19,6 @@ ABSL_DECLARE_FLAG(uint64_t, preserved_segments_outside_live_window);
 ABSL_DECLARE_FLAG(std::string, default_language);
 ABSL_DECLARE_FLAG(std::string, default_text_language);
 ABSL_DECLARE_FLAG(bool, force_cl_index);
+ABSL_DECLARE_FLAG(bool, per_playlist_target_duration);
 
 #endif  // PACKAGER_APP_MANIFEST_FLAGS_H_
